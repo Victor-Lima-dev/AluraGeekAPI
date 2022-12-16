062b652a120721706e68470afc7721edd90f55a7
VictorLima <victoraugustolima1@gmail.com>
2022-12-16 19:31:36 -0300

    Banco de dados criado com sucesso

---
+++ AluraGeekAPI/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using AluraGeekAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AluraGeekAPI.Context
+{
+    public class AppDbContext : DbContext
+    {
+        //implentar dbcontext
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        //mapear classe produto
+        public DbSet<Produto> Produtos { get; set; }
+
+    }
+}
---
+++ AluraGeekAPI/Models/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AluraGeekAPI.Models
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string? Nome { get; set; }
+        //range de valores
+        //definir tipo da coluna
+        
+        
+        [Range(1, 1000)]
+        public decimal? Preco { get; set; }
+        public string? Categoria { get; set; }
+    }
+}
