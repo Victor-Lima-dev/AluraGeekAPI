49443ecba539dfa9080e894757456b46b7b6b688
VictorLima <victoraugustolima1@gmail.com>
2022-12-16 22:01:05 -0300

    PostCarrinhoItem, incompleto

--- AluraGeekAPI/Controllers/CarrinhoCompraController.cs
+++ AluraGeekAPI/Controllers/CarrinhoCompraController.cs
@@ -2,6 +2,7 @@
 using AluraGeekAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AluraGeekAPI.Controllers
 {
@@ -20,17 +21,60 @@ namespace AluraGeekAPI.Controllers
 
         //metodo para retornar um carrinhoCompra assincrono
         //GET /carrinhoCompra/{id}
-        //[HttpGet("{id}")]
-        //public async Task<ActionResult<CarrinhoCompra>> GetCarrinhoCompra(int id)
-        //{
-        //    var carrinhoCompra = await _context.CarrinhoCompra.FindAsync(id);
+        [HttpGet("/Carrinho/{id}")]
+        public async Task<ActionResult<CarrinhoCompra>> GetCarrinhoCompra(int id)
+        {
+            var carrinhoCompra = await _context.CarrinhoCompras.FindAsync(id);
 
-        //    if (carrinhoCompra == null)
-        //    {
-        //        return NotFound();
-        //    }
+            if (carrinhoCompra == null)
+            {
+                return NotFound();
+            }
 
-        //    return carrinhoCompra;
-        //}
+            return Ok(carrinhoCompra);
+        }
+
+        //metodo para criar um carrinhoCompra assincrono
+        //POST /carrinhoCompra/Cadastrar
+        [HttpPost("Cadastrar")]
+        public async Task<ActionResult<CarrinhoCompra>> PostCarrinhoCompra(CarrinhoCompra carrinhoCompra)
+        {
+            _context.CarrinhoCompras.Add(carrinhoCompra);
+            await _context.SaveChangesAsync();
+
+            return Ok(carrinhoCompra);
+        }
+
+        //metodo para adicionar um carrinhoItem no carrinho
+        //POST /carrinhoCompra/AdicionarItem
+        [HttpPost("AdicionarItem")]
+        public async Task<ActionResult<CarrinhoCompra>> PostCarrinhoItem(Produto produto, int carrinhoId)
+        {
+
+            //refatora esse metodo, pois estamos criando um CarrinhoItem e adicionando ele no carrinhoCompra
+
+            //verifica se o item já existe no carrinho, se nao existir ele vai criar o item
+            var carrinhoItem = await _context.CarrinhoItens.FirstOrDefaultAsync(p => p.ProdutoId == produto.ProdutoId && 
+            p.CarrinhoItemId == carrinhoId);
+
+            if (carrinhoItem == null)
+            {
+                  carrinhoItem = new CarrinhoItem
+                {
+                    CarrinhoItemId = carrinhoId,
+                    ProdutoId = produto.ProdutoId,
+                    Quantidade = 1
+                };
+                _context.CarrinhoItens.Add(carrinhoItem);
+            }
+            
+            else
+            {
+                carrinhoItem.Quantidade++;
+            }
+            
+            await _context.SaveChangesAsync();
+            return Ok(carrinhoItem);
+        }
     }
 }
